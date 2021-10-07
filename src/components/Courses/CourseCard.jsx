d67dd6a3dad74fc38d9d441f80af9b5761bc534d
Yuliia Kyrychenko <juliiakyrychenko15@gmail.com>
2021-10-07 13:39:13 +0300

    add truncate box class

--- src/components/Courses/CourseCard.jsx
+++ src/components/Courses/CourseCard.jsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from 'react';
 import Button from '../Button/Button';
 import formatDuration from '../../mixins/format-duration';
 
 const CourseCard = ({ course, authors }) => {
-	const [description, setDescription] = useState(course.description);
-
-	const replaceTextWithEllipsis = () => {
-		// setDescription(text);
-	};
-
-	useEffect(() => {
-		replaceTextWithEllipsis();
-		window.addEventListener('resize', replaceTextWithEllipsis);
-		return () => {
-			window.removeEventListener('resize', replaceTextWithEllipsis);
-		};
-	});
-
 	return (
 		<article className='row align-items-start p-4 bg-light border'>
 			<div className='col-8 overflow-hidden'>
 				<h4>{course.title}</h4>
-				<p>{description}</p>
+				<p className='box-truncate'>{course.description}</p>
 			</div>
 			<div className='col-4'>
 				<div className='text-truncate'>
