d8bab577b8bf859256395b0b41ca98a0c27cde10
Kyrychenko, Yuliia <yuliia_kyrychenko@epam.com>
2023-06-01 19:16:30 +0300

    test develop commit

--- src/data/mockedCoursesList.js
+++ src/data/mockedCoursesList.js
@@ -10,17 +10,17 @@ const mockedCoursesList = [
 			'f762978b-61eb-4096-812b-ebde22838167',
 		],
 	},
-	{
-		id: 'b5630fdd-7bf7-4d39-b75a-2b5906fd0916',
-		title: 'Angular',
-		description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-		creationDate: '10/11/2020',
-		duration: 210,
-		authors: [
-			'df32994e-b23d-497c-9e4d-84e4dc02882f',
-			'095a1817-d45b-4ed7-9cf7-b2417bcbf748',
-		],
-	},
+	// {
+	// 	id: 'b5630fdd-7bf7-4d39-b75a-2b5906fd0916',
+	// 	title: 'Angular',
+	// 	description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+	// 	creationDate: '10/11/2020',
+	// 	duration: 210,
+	// 	authors: [
+	// 		'df32994e-b23d-497c-9e4d-84e4dc02882f',
+	// 		'095a1817-d45b-4ed7-9cf7-b2417bcbf748',
+	// 	],
+	// },
 ];
 
 export default mockedCoursesList;
